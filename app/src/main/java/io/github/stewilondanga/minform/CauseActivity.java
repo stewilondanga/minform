bd2ac5b030196ebeec5b1c7cfe79af1e2d56012f
Stewart ilondanga <stilondanga@gmail.com>
2018-01-28 17:07:47 +0300

    add app/src/main/java/io/github/stewilondanga/minform/CauseActivity.java

--- app/src/main/java/io/github/stewilondanga/minform/CauseActivity.java
+++ app/src/main/java/io/github/stewilondanga/minform/CauseActivity.java
@@ -1,13 +1,25 @@
 package io.github.stewilondanga.minform;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CauseActivity extends AppCompatActivity {
+    private Button mFindCausesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cause);
+
+        mFindCausesButton = (Button) findViewById(R.id.findCausesButton);
+        mFindCausesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
     }
 }
