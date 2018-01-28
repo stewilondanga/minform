0c1396a11908504aa76f04b22aaf99f2745d2d29
Stewart ilondanga <stilondanga@gmail.com>
2018-01-28 15:53:12 +0300

    add app/src/main/java/io/github/stewilondanga/minform/TimeActivity.java

--- app/src/main/java/io/github/stewilondanga/minform/ChangeActivity.java
+++ app/src/main/java/io/github/stewilondanga/minform/ChangeActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 
 public class ChangeActivity extends AppCompatActivity {
     private Button mFindCauseButton;
+    private Button mFindTimeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +20,15 @@ public class ChangeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ChangeActivity.this, CauseActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mFindTimeButton = (Button) findViewById(R.id.findTimeButton);
+        mFindTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChangeActivity.this, TimeActivity.class);
                 startActivity(intent);
             }
         });
