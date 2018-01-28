a663682be63f97a7cb966ef5d0c8dfabddfc0c17
Stewart ilondanga <stilondanga@gmail.com>
2018-01-28 17:25:45 +0300

    add app/src/main/java/io/github/stewilondanga/minform/TimeActivity.java

--- app/src/main/java/io/github/stewilondanga/minform/TimeActivity.java
+++ app/src/main/java/io/github/stewilondanga/minform/TimeActivity.java
@@ -1,13 +1,25 @@
 package io.github.stewilondanga.minform;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TimeActivity extends AppCompatActivity {
+    private Button mFindTimeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time);
+
+        mFindTimeButton = (Button) findViewById(R.id.findTimeButton);
+        mFindTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
     }
 }
