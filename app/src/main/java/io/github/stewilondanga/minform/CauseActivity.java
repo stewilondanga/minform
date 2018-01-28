146e6af546033e8e230af758dbc6deb474b90d73
Stewart ilondanga <stilondanga@gmail.com>
2018-01-28 15:47:28 +0300

    add app/src/main/java/io/github/stewilondanga/minform/CauseActivity.java

---
+++ app/src/main/java/io/github/stewilondanga/minform/CauseActivity.java
@@ -0,0 +1,13 @@
+package io.github.stewilondanga.minform;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CauseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cause);
+    }
+}
--- app/src/main/java/io/github/stewilondanga/minform/ChangeActivity.java
+++ app/src/main/java/io/github/stewilondanga/minform/ChangeActivity.java
@@ -1,13 +1,26 @@
 package io.github.stewilondanga.minform;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChangeActivity extends AppCompatActivity {
+    private Button mFindCauseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change);
+
+        mFindCauseButton = (Button) findViewById(R.id.findCauseButton);
+        mFindCauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChangeActivity.this, CauseActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
