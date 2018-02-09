7216d42d01eda0c06cfa57ba4f124f9b9e82c678
Stewart ilondanga <stilondanga@gmail.com>
2018-02-09 15:48:55 +0300

    add app/src/main/java/io/github/stewilondanga/minform/TimeActivity.java

--- app/src/main/java/io/github/stewilondanga/minform/TimeActivity.java
+++ app/src/main/java/io/github/stewilondanga/minform/TimeActivity.java
@@ -15,29 +15,62 @@ public class TimeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time);
 
+        /**
+         * This method is called when the button is clicked
+         */
+
         mFindTimeButton = (Button) findViewById(R.id.findTimeButton);
         mFindTimeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 CheckBox checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
+                boolean oneHour = checkBox1.isChecked();
+
                 CheckBox checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
+                boolean twoToFiveHours = checkBox2.isChecked();
+
                 CheckBox checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
+                boolean fivePlusHours = checkBox3.isChecked();
+
+
             }
         });
+
+        /**
+         * This method is called when checkbox 1 is clicked
+         */
 
         final CheckBox checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
         if (checkBox1.isChecked()) {
             checkBox1.setChecked(false);
         }
 
+        /**
+         * This method is called when checkbox 2 is clicked
+         */
+
         final CheckBox checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
         if (checkBox2.isChecked()) {
             checkBox2.setChecked(false);
         }
 
+        /**
+         * This method is called when checkbox 3 is clicked
+         */
+
         final CheckBox checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
         if (checkBox3.isChecked()) {
             checkBox3.setChecked(false);
         }
+
+    }
+
+        /**
+        * Create Cause summary
+        */
+
+        private String createCauseSummary(boolean oneHour, boolean twoToFiveHours, boolean fivePlusHours) {
+
+            return null;
         }
 }
